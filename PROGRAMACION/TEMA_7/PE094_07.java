69c99f649de23a04ff3b97467a27b17dd28aa44f
FernandoLeivaBrenes <fernando.leiva.brenes@gmail.com>
2018-11-20 11:33:30 +0100

    Updated.

--- PROGRAMACION/TEMA_7/PE094_07.java
+++ PROGRAMACION/TEMA_7/PE094_07.java
@@ -1,30 +1,63 @@
 public class PE094_07{
 	public static void main(String[] args){
 		int[] numero= new int[100];
-		int contador = 0, contador1 = 1, addnumeracion = 0 ;
-		String numeracion = "", salto = "";
+		int contador = 0, varMixt= 0,varMixtAr[] = new int[2];
+		String numeracion = "";
 		
 		do{
-			addnumeracion = (int)(Math.random()*21);
-			numeracion += " "+addnumeracion;
-			numero[contador] = addnumeracion;
+			varMixt = (int)(Math.random()*21);
+			numeracion += " "+varMixt;
+			numero[contador] = varMixt;
 			contador++;
 		}while(contador<numero.length);
-
-		for(int i = numero.length-1 ; i>0; i--){
-			numero[i] = numero[i-1];
-			}
-
-		System.out.println("\n"+ numeracion);
+		
+		contador = 1;
+		varMixt = 0;
 		for(int i: numero){
 
-			if( ((contador1%10) == 0)){
+			if( ((contador%10) == 0)){
 				System.out.printf(" %4d\n",i );
 				}else{
 					System.out.printf(" %4d",i );
 					}
 					
-			contador1++;
+			contador++;
 			}
+			
+		for( int i= 0; i<=1; i++){
+			System.out.print("\nIntroduzca la posicion "+(i+1)+" : ");
+			varMixtAr[i] = Integer.parseInt(System.console().readLine());
+			}
+		
+		if((varMixtAr[0]>=0 || varMixtAr[0]<=99)&&(varMixtAr[1]>=0 || varMixtAr[1]<=99)){
+			varMixt = numero[varMixtAr[0]];
+			numero[varMixtAr[0]] = numero[varMixtAr[1]];
+			numero[varMixtAr[1]] = varMixt;	
+			} else{
+				System.out.println("Error. You need to introduce a numero between 0 and 99.");
+				}
+				
+		contador = 1;
+		for(int i: numero){
+			if( ((contador-1)==varMixtAr[0]) || ((contador-1)==varMixtAr[1]) ){
+				
+				if( ((contador%10) == 0)){
+					System.out.print(" '"+i+"'\n");
+					}else{
+						System.out.print(" '"+i+"'");
+					}
+
+				}else{
+					
+				if( ((contador%10) == 0)){
+					System.out.printf(" %4d\n",i );
+					}else{
+						System.out.printf(" %4d",i );
+					}
+					
+				}
+			contador++;
+			}
+			
 		}
 	}
